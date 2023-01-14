8a93ec38acb432aa8e75455bf1e8f08fb001c82a
Yogesh188 <yogesh.kumbhar188@gmail.com>
2023-01-14 12:23:04 +0530

    Rewritten shorter code

--- app.js
+++ app.js
@@ -8,58 +8,12 @@ var outputDiv = document.querySelector("#outputDiv");
 function operation(operator){
     var userInput1=document.querySelector("#userInput1");
     var userInput2=document.querySelector("#userInput2");
-    // var answer=eval(userInput1+operator+userInput2);
-    // outputDiv.innertext(answer);
-    var answer=userInput1+userInput2;
-    return answer;
-}
-
-function clickHandler1(){
-    // var userInput1=document.querySelector("#userInput1");
-    // var userInput2=document.querySelector("#userInput2");
-    // outputDiv.innerText="";
-    // userInput1=Number(userInput1.value);
-    // userInput2=Number(userInput2.value);
-    // var answer=userInput1+userInput2;
-    // outputDiv.innerText = answer;
-    var answer = operation()
-    outputDiv.innertext(operation("+"));
-}
-
-function clickHandler2(){
-    var userInput1=document.querySelector("#userInput1");
-    var userInput2=document.querySelector("#userInput2");
-    outputDiv.innerText="";
-    userInput1=Number(userInput1.value);
-    userInput2=Number(userInput2.value);
-    var answer=userInput1-userInput2;
-    outputDiv.innerText = Number(answer);
-}
-
-
-function clickHandler3(){
-    var userInput1=document.querySelector("#userInput1");
-    var userInput2=document.querySelector("#userInput2");
-    outputDiv.innerText="";
-    userInput1=Number(userInput1.value);
-    userInput2=Number(userInput2.value);
-    var answer=userInput1*userInput2;
-    outputDiv.innerText = Number(answer);
-}
-
-
-function clickHandler4(){
-    var userInput1=document.querySelector("#userInput1");
-    var userInput2=document.querySelector("#userInput2");
-    outputDiv.innerText="";
-    userInput1=Number(userInput1.value);
-    userInput2=Number(userInput2.value);
-    var answer=userInput1/userInput2;
-    outputDiv.innerText = Number(answer);
+    var answer= eval(Number(userInput1.value)+operator+Number(userInput2.value));
+    outputDiv.innerText=(Number(answer));
 }
 
 //Get the click and start process (Call clickhandlers)
-btn1.addEventListener("click",clickHandler1);
-btn2.addEventListener("click",clickHandler2);
-btn3.addEventListener("click",clickHandler3);
-btn4.addEventListener("click",clickHandler4);
+btn1.addEventListener("click",function func(){operation("+")});
+btn2.addEventListener("click",function func(){operation("-")});
+btn3.addEventListener("click",function func(){operation("*")});
+btn4.addEventListener("click",function func(){operation("/")});
